e0346971f0348253fc982d60e052f0bbfe9c03e9
muratcan-yuksel <muratcanyukselpro@gmail.com>
2024-04-16 17:50:55 +0700

    succesfully displayed lender info

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,25 @@ import { Box } from "@mui/material";
 import NavbarComponent from "./components/NavbarComponent.jsx";
 import CardComponent from "./components/CardComponent.jsx";
 import "./App.css";
+import {
+  lendingProtocolAbi,
+  protocol_contract_address,
+} from "./ABI/lendingProtocolAbi.js";
+import { useReadContract, useAccount } from "wagmi";
 const App = () => {
+  const { address, isConnected } = useAccount();
+  //lender functions
+  console.log(address);
+
+  const lenderinfo = useReadContract({
+    abi: lendingProtocolAbi,
+    address: protocol_contract_address,
+    functionName: "getLenderInfo",
+    args: [address],
+  });
+  console.log(lenderinfo.data);
+
+  //borrower functions
   return (
     <Box>
       <NavbarComponent />
@@ -14,8 +32,10 @@ const App = () => {
           border: "1px solid red",
         }}
       >
-        <CardComponent />
-        <CardComponent />
+        {/* lender */}
+        <CardComponent userInfo={lenderinfo.data} />
+        {/* borrower */}
+        {/* <CardComponent /> */}
       </Box>
     </Box>
   );
--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,14 +1,49 @@
+/* eslint-disable react/prop-types */
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { formatEther } from "viem";
 
-export default function BasicCard() {
+export default function CardComponent({ userInfo }) {
+  console.log(userInfo);
+  console.log(userInfo.depositTime);
+  console.log(userInfo.depositTime);
+
+  const giveTimestamp = (timestamp) => {
+    const date = new Date(Number(timestamp) * 1000);
+    return date.toLocaleString();
+  };
+
   return (
     <Card sx={{ minWidth: 275 }}>
-      <CardContent></CardContent>
+      <CardContent>
+        {userInfo && (
+          <Box>
+            <Typography
+              sx={{ fontSize: 14 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              Amount of LPT lent to the protocol:{" "}
+              {formatEther(userInfo.amountLent)}
+            </Typography>
+            <Typography
+              sx={{ fontSize: 14 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              Interst Earned: {formatEther(userInfo.interestEarned)}
+            </Typography>{" "}
+            <Typography sx={{ fontSize: 14 }} gutterBottom>
+              {userInfo.depositTime &&
+                `Deposit Time: ${giveTimestamp(userInfo.depositTime)}`}{" "}
+            </Typography>
+          </Box>
+        )}
+      </CardContent>
       <CardActions>
         <Button size="small">Learn More</Button>
       </CardActions>
